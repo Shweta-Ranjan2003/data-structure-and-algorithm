6863ab1ab1f672bfc6367bf8b159d630064daff6
Shweta <ranjanshweta2203@gmail.com>
2022-07-13 16:16:45 +0530

    ds

--- DS Notes/BinaryTree.cpp
+++ DS Notes/BinaryTree.cpp
@@ -34,8 +34,117 @@ right child index = 2i+2
 /*
 Binary Search Tree -> A binary tree in which for each node , value of all the nodes in left subtree is lesser or equal and value of all the nodes in right subtree is greater.
 */
+struct BstNode
+{
+    int data;
+    BstNode* left;
+    BstNode* right;
+};
+BstNode* GetNewNode(int data){
+    BstNode* newNode = new BstNode();
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+//Refer video 6:12hr freecodecamp for both meathods of below codes.
+// void Insert(BstNode** root , int data){
+//  if (*root==NULL)
+//  {
+//     *root= GetNewNode(data);
+//  }
+ 
+// }
+//Function to insert in BST , returns address of root node
+BstNode* Insert(BstNode* root , int data){
+    if (root==NULL)
+    {
+        root = GetNewNode(data);
+    }
+    else if (data<= root->data)
+    {
+        root->left = Insert(root->left,data);
+    }
+    else{
+        root->right = Insert(root->right,data);
+    }
+        return root;
+}
+bool Search(BstNode* root , int data){
+    if (root==NULL) return false;
+     else if(root->data==data) return true;
+     else if(data<=root->data) return Search(root->left,data);
+     else return Search(root->right,data);
+    }
+int FindMin(BstNode* root){
+    if (root==NULL)
+    {
+        cout<<"Error: tree is empty"<<endl;
+        return -1;
+    }
+    //By recursion
+    // else if (root->left==NULL)
+    // {
+    //     return root->data;
+    // }
+    // return FindMin(root->left);
+    while (root->left!=NULL)
+    {
+        root=root->left;
+    }
+    return root->data;
+}
+int FindMax(BstNode* root){
+    if (root==NULL)
+    {
+        cout<<"Error: tree is empty"<<endl;
+        return -1;
+    }
+    //By recursion
+    else if (root->right==NULL)
+    {
+        return root->data;
+    }
+    return FindMax(root->right);
+    // while (root->right!=NULL)
+    // {
+    //     root=root->right;
+    // }
+    // return root->data;
+}
+//Height of a node: Number of edges in longest path from the node to a leaf node
+//height of tree = height of root
+//height of tree with 1 node = 0;
+//Depth of a node = No. of edges in path from root to that node.
+int FindHeight(BstNode* root){
+    if (root==NULL)
+    {
+        return -1;
+    }
+    return max(FindHeight(root->left),FindHeight(root->right))+1;
+}
 int main()
 { 
+BstNode* root = NULL;
+// Insert(&root , 15); 
+// Insert(&root,10);
+// Insert(&root,20);
+root = Insert(root,15);
+root = Insert(root,10);
+root = Insert(root,20);
+root = Insert(root,25);
+root = Insert(root,8);
+root = Insert(root,12);
+int a=FindMin(root);
+int b=FindMax(root);
+cout<<"Min: "<<a<<" Max: "<<b<<endl;
+ int number;
+ cout<<"enter number to search"<<endl;
+ cin>>number;
+ if (Search(root,number)==true)
+ {
+    cout<<"found"<<endl;
+ }
+ else cout<<"not found"<<endl;
  
  return 0;
 }
